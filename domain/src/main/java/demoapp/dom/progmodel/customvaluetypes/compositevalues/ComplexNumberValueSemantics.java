b59dcd78ff73df72c750471a60e9d944008923c5
Brian Kalbfus <harvestmoon299@gmail.com>
2025-03-07 12:36:53 -0900

    Revert "Better implementation of ComplexNumberValueSemantics"
    This reverts commit 3f52d906d101b90b8bab76f6f58a1d8e44a4dc97.

--- domain/src/main/java/demoapp/dom/progmodel/customvaluetypes/compositevalues/ComplexNumberValueSemantics.java
+++ domain/src/main/java/demoapp/dom/progmodel/customvaluetypes/compositevalues/ComplexNumberValueSemantics.java
@@ -66,13 +66,12 @@ public class ComplexNumberValueSemantics
 
     @Override
     public ComplexNumber compose(final ValueDecomposition decomposition) {
-        return decomposition.right()
-    	   .map(CommonDtoUtils::typedTupleAsMap)
-    	   .map(map->ComplexNumber.of(
-    			   (Double)map.get("re"),
-    			   (Double)map.get("im")
-    			   ))
-    	   .orElse(null);
+    	TypedTupleBuilder<Optional<TypedTupleDto>> right = CommonDtoUtils.typedTupleBuilder(decomposition.right());
+    	TypedTupleDto tupleDto = right.build();
+    	Map<String, Object> map = CommonDtoUtils.typedTupleAsMap(tupleDto);
+    	ComplexNumber cn = ComplexNumber.of((Double)map.get("re"), (Double) map.get("im"));
+
+        return cn;
     }
 //end::compose[]
 
